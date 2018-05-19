775acc4d138e89c9db03d378e1fc43dce36f65d9
Richard Ahlin <richard@Richards-MacBook-Pro.local>
2018-05-19 13:10:04 +0200

    now calculates highscores partly

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 const MongoClient = require('mongodb').MongoClient;
-
 const express = require('express');
 const WebSocket = require('ws');
 
-// Connect to Database
 const dbhost = "mongodb://localhost:27017/";
 var db;
+
 MongoClient.connect(dbhost, function(err, database) {
     if (err) throw err;
-    db = database.db('tidifydb');
+    db = database.db('mydb');
     console.log("Database connected!");
 });
 
-// Start websocket server.
 const wss = new WebSocket.Server({port: 3001});
 
-// Define a broadcast function.
 wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN) {
@@ -25,7 +22,6 @@ wss.broadcast = function broadcast(data) {
 };
 
 var app = express()
-
 // make our db and wss accessible to our router
 app.use(function(req, res, next) {
     req.db = db;
@@ -33,17 +29,12 @@ app.use(function(req, res, next) {
     next();
 });
 
-// Start mounting endpoints
-//
+var users = require('./users');
+app.use('/users', users);
 
-var ep_activities = require('./endpoints/activities');
-var ep_scoreboard = require('./endpoints/scoreboard');
-
-app.use('/activities', ep_activities);
-app.use('/scoreboard', ep_scoreboard);
-
-app.get('/helloworld', (req, res) => {
-    res.send({msg: "Hello World!"})
+app.get('/', (req, res) => {
+    var collection = db.collection('users');
+    res.send({msg: "Index test!"})
 } )
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
